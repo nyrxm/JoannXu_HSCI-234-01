cb32a732d7da75e755512c5ca8aef365942e9f20
nyrxm <123412270+nyrxm@users.noreply.github.com>
2023-02-05 03:54:03 -0800

    Update main.py

--- main.py
+++ main.py
@@ -6,13 +6,16 @@ def setup():
 
 def draw():
     p5.background(255)           # white background
-        
+    
     p5.noStroke()  # <3<3<3
     p5.fill(200, 0, 0)
     for i in range(10):
-        p5.arc(i * 30 , i * 30, 20, 20, 3.14, 0)
-        p5.arc(i * 30 + 20, i *30, 20, 20, 3.14, 0)
-        p5.triangle(i * 30 - 10, i * 30, i* 30 + 30 , i * 30, i * 30 + 10, i * 30 + 20)
+        p5.arc(i * 60 , 30, 20, 20, 3.14, 0)
+        p5.arc(i * 60 + 20, 30, 20, 20, 3.14, 0)
+        p5.triangle(i * 60 - 10, 30, i* 60 + 30, 30, i * 60 + 10, 50)
+        p5.arc(i * 60 , 260, 20, 20, 3.14, 0)
+        p5.arc(i * 60 + 20, 260, 20, 20, 3.14, 0)
+        p5.triangle(i * 60 - 10, 260, i* 60 + 30, 260, i * 60 + 10, 280)
 
     p5.fill(240, 200, 200)  # pink body part
     p5.arc(100, 100, 50, 50, 3.14, 0)
