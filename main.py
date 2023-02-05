05b8f25a92bdbed9de2879da565db71c64961525
nyrxm <123412270+nyrxm@users.noreply.github.com>
2023-02-05 03:44:28 -0800

    Update main.py

--- main.py
+++ main.py
@@ -6,13 +6,15 @@ def setup():
 
 def draw():
     p5.background(255)           # white background
-    p5.noStroke()
+        
+    p5.noStroke()  # <3<3<3
     p5.fill(200, 0, 0)
-    p5.arc(10, 10, 20, 20, 3.14, 0)
-    p5.arc(30, 10, 20, 20, 3.14, 0)
-    p5.triangle(0, 10, 40, 10, 20, 30)
-    
-    p5.fill(240, 200, 200)
+    for i in range(10):
+        p5.arc(i * 30 , i * 30, 20, 20, 3.14, 0)
+        p5.arc(i * 30 + 20, i *30, 20, 20, 3.14, 0)
+        p5.triangle(i * 30 - 10, i * 30, i* 30 + 30 , i * 30, i * 30 + 10, i * 30 + 20)
+
+    p5.fill(240, 200, 200)  # pink body part
     p5.arc(100, 100, 50, 50, 3.14, 0)
     p5.arc(200, 100, 50, 50, 3.14, 0)
     p5.rect(75, 100, 50, 25)
@@ -20,7 +22,7 @@ def draw():
     p5.rect(75, 125, 150, 75)
     p5.arc(75, 175, 75, 50, 1.57, -1.57)
 
-    p5.strokeWeight(5)
+    p5.strokeWeight(5)  # UwU eyes
     p5.stroke(0)
     p5.arc(110, 150, 50, 60, 3.14, 0)
     p5.arc(185, 150, 50, 60, 3.14, 0)
@@ -32,7 +34,7 @@ def draw():
     p5.ellipse(130, 160, 5, 10)
     p5.ellipse(205, 160, 5, 10)
 
-    p5.noFill()
+    p5.noFill()  # UwU mouth
     p5.strokeWeight(3)
     p5.stroke(100, 50, 50)
     p5.line(140, 175, 145, 180)
@@ -40,18 +42,15 @@ def draw():
     p5.line(150, 175, 155, 180)
     p5.line(155, 180, 160, 175)
 
-    p5.strokeWeight(1)
+    p5.strokeWeight(1)  #  hands
     p5.stroke(0)
     p5.fill(255, 255, 255)
     p5.ellipse(135, 200, 25, 25)
     p5.ellipse(165, 200, 25, 25)
 
-    p5.strokeWeight(6)
+    p5.strokeWeight(6)  #  blush
     p5.stroke(255, 0, 0)
     p5.noFill(0)
-    p5.line(75, 165, 60, 185)
-    p5.line(85, 165, 70, 185)
-    p5.line(95, 165, 80, 185)
-    p5.line(215, 165, 200, 185)
-    p5.line(225, 165, 210, 185)
-    p5.line(235, 165, 220, 185)
+    for i in range(3):
+        p5.line(75 + i * 10, 165, 60 + i * 10, 185)
+        p5.line(215 + i * 10, 165, 200 + i * 10, 185)
